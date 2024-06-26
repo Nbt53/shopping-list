b0145c4843a0298129a56be6e147c85fb3597dd2
nbt53 <nbt853.nh@gmail.com>
2024-06-26 17:39:30 +0100

    removed firebase config

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,35 @@
 <template>
   <div id="app">
-    <div id="container">
+    <div id="container" v-if="loggedIn">
       <div class="content">
-             <router-view />
+        <router-view />
       </div>
-      <button @click="login">Login with Google</button>
       <nav-bar />
+    </div>
+    <div v-else>
+      <log-in />
     </div>
   </div>
 </template>
 
 <script>
+import { ref } from "vue";
 import NavBar from "./components/nav-bar.vue";
-import { useLogin } from "./composables/user/useLogin";
+import useIsLoggedIn from "./composables/user/isLoggedIn";
+import LogIn from "./components/user/LogIn.vue";
 
 export default {
   components: {
     NavBar,
+    LogIn,
   },
 
   setup() {
-    const { loginWithGoogle } = useLogin();
+    const { isLoggedIn } = useIsLoggedIn();
+    const loggedIn = ref(isLoggedIn);
+  
 
-    const login = async () => {
-      try {
-        const result = await loginWithGoogle();
-        console.log(result);
-        // Proceed with result, e.g., redirect or update UI
-      } catch (error) {
-        console.error('Login failed:', error);
-      }
-    };
-
-    return { login };
+    return { loggedIn };
   },
 };
 </script>
---
+++ src/components/user/LogIn.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="login">
+    <div class="login-box">
+      <h1>Super cool Shopping list</h1>
+      <button @click="login">Log in with Google</button>
+    </div>
+  </div>
+</template>
+
+<script>
+import { useLogin } from '../../composables/user/useLogin';
+export default {
+    setup() {
+        const { loginWithGoogle } = useLogin();
+
+        const login = async () => {
+            try {
+                const result = await loginWithGoogle();
+                console.log(result);
+            } catch (error) {
+                console.error("Login failed:", error);
+            }
+        };
+        return { login };
+    } // This closing brace was missing
+};
+</script>
+
+<style>
+.login {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  height: 100vh;
+  width: 100%;
+  text-align: center;
+}
+
+.login-box {
+  margin: auto;
+  padding: 20px;
+  border: 1px solid black;
+  border-radius: 5px;
+  width: 300px;
+  background-color: var(--color-primary);
+}
+
+.login-box h1 {
+  margin: 0 0 2rem 0;
+}
+</style>
